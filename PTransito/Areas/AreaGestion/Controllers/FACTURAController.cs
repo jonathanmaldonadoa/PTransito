7013e45cffbc9470410f5450c75d81f9286b92d5
Jonathan Maldonado <jonathan-paginas@hotmail.com>
2019-08-15 01:39:54 -0500

    separando servicio

--- PTransito/Areas/AreaGestion/Controllers/FACTURAController.cs
+++ PTransito/Areas/AreaGestion/Controllers/FACTURAController.cs
@@ -1,16 +1,19 @@
-﻿using System.Data.Entity;
+﻿using System;
+using System.Data.Entity;
 using System.Linq;
 using System.Net;
 using System.Threading.Tasks;
 using System.Web.Mvc;
 using TransitoModel;
 using TransitoModel.ModelAux;
+using TransitoServicio;
 
 namespace PTransito.Areas.AreaGestion.Controllers
 {
     public class FACTURAController : Controller
     {
         private PRUEBA1Entities db = new PRUEBA1Entities();
+        private SERVICIO_FACTURA sf = new SERVICIO_FACTURA();
 
         public async Task<ActionResult> Index()
         {
@@ -20,16 +23,14 @@ namespace PTransito.Areas.AreaGestion.Controllers
 
         public async Task<ActionResult> Details(decimal id)
         {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            FACTURA fACTURA = await db.FACTURA.FindAsync(id);
-            if (fACTURA == null)
+            var aux = await sf.FacturaDetalle(id);
+            if (aux == null)
             {
                 return HttpNotFound();
             }
-            return View(fACTURA);
+            ViewBag.total = aux.FACTURA_TRAMITE.Sum(x=>x.TRAMITE.VALOR);
+            ViewBag.ID = new SelectList(db.TRAMITE, "ID", "DESCRIPCION");
+            return View(aux);
         }
 
         public ActionResult Create()
@@ -37,23 +38,37 @@ namespace PTransito.Areas.AreaGestion.Controllers
             ViewBag.contador = 0;
             ViewBag.ID_VEHICULO = new SelectList(db.VEHICULO, "ID", "PLACA");
             ViewBag.ID_TRAMITE = new SelectList(db.TRAMITE, "ID", "DESCRIPCION");
-
             return View("Create");
         }
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<ActionResult> Buscar(ModFactura m)
+        public ActionResult Buscar(ModFactura m)
         {
-            var bus = await db.PROPIETARIO_VEHICULO
-                 .Include(x => x.VEHICULO)
-                 .Include(x => x.PROPIETARIO)
-                 .Where(x => x.PROPIETARIO.NUMEROIDENTIFICACION == m.DOCUMENTO && x.VEHICULO.PLACA == m.PLACA).ToListAsync();
+            ViewBag.contador = sf.Contar(m);
             ViewBag.ID_VEHICULO = new SelectList(db.VEHICULO.Where(x => x.PLACA == m.PLACA), "ID", "PLACA");
-            ViewBag.contador = bus.Count();
             ViewBag.ID = new SelectList(db.TRAMITE, "ID", "DESCRIPCION");
 
             return View("create");
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> AgregarTramite(ModFactura m)
+        {
+            if(await sf.AgregarTramite(m))
+            {
+                ViewBag.ID = new SelectList(db.TRAMITE, "ID", "DESCRIPCION");
+                return RedirectToAction("Details", new { id = m.FACTURA.ID });
+            }
+            else
+            {
+                ViewBag.ID = new SelectList(db.TRAMITE, "ID", "DESCRIPCION");
+                return RedirectToAction("Details", new { id = m.FACTURA.ID });
+            }
+
+            
         }
 
         [HttpPost]
@@ -62,21 +77,11 @@ namespace PTransito.Areas.AreaGestion.Controllers
         {
             if (ModelState.IsValid)
             {
-                FACTURA_TRAMITE aux = new FACTURA_TRAMITE();
-                aux.ID_TRAMITE = m.TRAMITE.ID;
-                FACTURA aux2 = new FACTURA();
-                aux2.FACTURA_TRAMITE.Add(aux);
-                aux2.FECHA = m.FACTURA.FECHA;
-                aux2.ID_VEHICULO = m.FACTURA.ID_VEHICULO;
-                aux2.NUMERO =m.FACTURA.NUMERO;
-                aux2.OBSERVACIONES = m.FACTURA.OBSERVACIONES;
-                db.FACTURA.Add(aux2);
-                await db.SaveChangesAsync();
-                return RedirectToAction("Index");
+                decimal aux = await sf.CrearFactura(m);
+                if (aux>0)
+                    return RedirectToAction("Details", new { id = aux });
             }
-
-         //   ViewBag.ID_VEHICULO = new SelectList(db.VEHICULO, "ID", "PLACA", fACTURA.ID_VEHICULO);
-            return View();
+            return View("Buscar");
         }
 
 
--- TransitoServicio/SERVICIO_FACTURA.cs
+++ TransitoServicio/SERVICIO_FACTURA.cs
@@ -1,12 +1,85 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
+using TransitoModel;
+using TransitoModel.ModelAux;
+
 
 namespace TransitoServicio
 {
     public class SERVICIO_FACTURA
     {
+        private PRUEBA1Entities db = new PRUEBA1Entities();
+
+        public int Contar(ModFactura m)
+        {
+            var bus = db.PROPIETARIO_VEHICULO
+                .Include(x => x.VEHICULO)
+                .Include(x => x.PROPIETARIO)
+                .Where(x => x.PROPIETARIO.NUMEROIDENTIFICACION == m.DOCUMENTO && x.VEHICULO.PLACA == m.PLACA).ToList();
+            if (bus.Count() > 0)
+            {
+                return bus.Count();
+            }
+            return 0;
+        }
+
+        public async Task<bool> AgregarTramite(ModFactura m)
+        {
+            try
+            {
+                FACTURA_TRAMITE aux = new FACTURA_TRAMITE();
+                aux.ID_TRAMITE = m.TRAMITE.ID;
+                aux.CREA_FECHA = DateTime.Now;
+                aux.ID_FACTURA = m.FACTURA.ID;
+                db.FACTURA_TRAMITE.Add(aux);
+                await db.SaveChangesAsync();
+                return true;
+            }
+            catch
+            {
+                return false;
+
+            }
+        }
+        public async Task<decimal> CrearFactura(ModFactura m)
+        {
+            try
+            {
+                    FACTURA_TRAMITE aux = new FACTURA_TRAMITE();
+                    aux.ID_TRAMITE = m.TRAMITE.ID;
+                    aux.CREA_FECHA = DateTime.Now;
+
+                    FACTURA aux2 = new FACTURA();
+                    aux2.FACTURA_TRAMITE.Add(aux);
+                    aux2.FECHA = m.FACTURA.FECHA;
+                    aux2.ID_VEHICULO = m.FACTURA.ID_VEHICULO;
+                    aux2.NUMERO = m.FACTURA.NUMERO;
+                    aux2.OBSERVACIONES = m.FACTURA.OBSERVACIONES;
+
+                    db.FACTURA.Add(aux2);
+                    await db.SaveChangesAsync();
+                    decimal ID = aux2.ID;
+
+                    return ID;
+            }
+            catch
+            {
+                return 0;
+
+            }
+        }
+
+        public async Task<ModFactura> FacturaDetalle(decimal m)
+        {
+            FACTURA fACTURA = await db.FACTURA.FindAsync(m);
+            ModFactura aux = new ModFactura();
+            aux.FACTURA = fACTURA;
+            aux.FACTURA_TRAMITE = fACTURA.FACTURA_TRAMITE;
+
+            return aux;
+        }
+
     }
 }
